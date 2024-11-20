01e7ed330bb01269fd50681bdbbe85e7afcb826f
Nailton Santos <nailtonsdelima@gmail.com>
2024-11-20 13:16:59 -0300

    Criação do projeto

---
+++ src/main/java/com/nailton/filmeslist/FilmeslistApplication.java
@@ -0,0 +1,13 @@
+package com.nailton.filmeslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmeslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmeslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nailton/filmeslist/FilmeslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nailton.filmeslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmeslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
