c3e010be822932399c8b2642e6b1b21626c11984
Nailton Santos <nailtonsdelima@gmail.com>
2024-12-07 00:21:40 -0300

    Projections, trazendo itens com identificadores de categorias

--- src/main/java/com/nailton/filmeslist/repositories/GameRepository.java
+++ src/main/java/com/nailton/filmeslist/repositories/GameRepository.java
@@ -1,9 +1,24 @@
 package com.nailton.filmeslist.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.nailton.filmeslist.entities.Game;
+import com.nailton.filmeslist.projections.GameMinProjection;
 
 public interface GameRepository extends JpaRepository<Game, Long>{
+	
+	/* Para usar uma query nativa do SQL é necessário usar o nativeQuery=true abaixo */
+	@Query(nativeQuery = true, value = """
+			SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+			tb_game.short_description AS shortDescription, tb_belonging.position
+			FROM tb_game
+			INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
+			WHERE tb_belonging.list_id = :listId
+			ORDER BY tb_belonging.position
+				""")
+	List<GameMinProjection> searchByList(Long listId);
 
 }
--- src/main/java/com/nailton/filmeslist/services/GameService.java
+++ src/main/java/com/nailton/filmeslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.nailton.filmeslist.dto.GameDTO;
 import com.nailton.filmeslist.dto.GameMinDTO;
 import com.nailton.filmeslist.entities.Game;
+import com.nailton.filmeslist.projections.GameMinProjection;
 import com.nailton.filmeslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,13 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
